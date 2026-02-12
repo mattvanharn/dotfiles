18d2fa46e52573c1fb4a77911ab0c30d944de49d
Matt Van Harn <mattvanharn@gmail.com>
2026-02-12 15:25:05 -0500

    feat: fresh dotfiles setup with stow
    - home: zshrc and p10k configs
    - config: bat config, kitty custom overrides
    - nvim: minimal init.lua (to be built out later)
    - bin: install, uninstall, restow, and post-hypr-update scripts

---
+++ nvim/init.lua
@@ -0,0 +1,10 @@
+-- Neovim configuration
+-- TODO: Set up LazyVim and plugins
+
+-- Basic settings for now
+vim.opt.number = true         -- line numbers
+vim.opt.relativenumber = true -- relative line numbers
+vim.opt.expandtab = true      -- spaces instead of tabs
+vim.opt.shiftwidth = 2        -- indent width
+vim.opt.tabstop = 2           -- tab width
+vim.opt.clipboard = "unnamedplus" -- use system clipboard
